7d5d09f8cb0adec9e57d8b6105a75f4c8dbe1e43
Nathan Maillot <nathan.maillot@epitech.eu>
2026-02-04 07:01:51 +0900

    fix(debug) try to set miplevel at 1

--- sources/AApp.cpp
+++ sources/AApp.cpp
@@ -1261,7 +1261,6 @@ void etib::AApp::createImage(uint32_t width, uint32_t height, uint32_t mipLevels
     imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
     imageInfo.usage = usage;
     imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
-    imageInfo.mipLevels = mipLevels;
     imageInfo.samples = numSamples;
 
     if (vkCreateImage(_logicalDevice, &imageInfo, nullptr, &image) != VK_SUCCESS) {
